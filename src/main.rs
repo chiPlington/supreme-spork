a21bf08906e6354b891a65d530cb546a6d4687f0
rustaceanplantation <nickwolkow@gmail.com>
2018-10-08 22:53:52 -0700

    Discovered the fun of Vec<T>
    Might come back to finish later.

--- src/main.rs
+++ src/main.rs
@@ -3,25 +3,29 @@
 
 fn main() {
 
-    let c = 0;
-    let primes: Vec<u32> = Vec::new();   //list of primes
-
-    println!("Give me a number");
+  
+    let mut primes: Vec<u32> = vec![0, 1, 2];   //list of primes
+    
+    println!("Give me a max number...");
     let i: i32 = read!();  //read in user defined max number
 
-    for c in 0..i+1 {  //iterate from 0 to chosen number(i)
-        for d in 0..primes.len()+1 {
-            
+    for _c in 0..i+1 {  //iterate from 0 to chosen number(i)
+        for d in 2..primes.len() {
+            if d as u32 % &primes[d] < 1 {
+                primes.push(d as u32);
+            };
         }   
         
         
     }    
     
     
+    for _c in 0..primes.len() {
+        println!("{}", primes[_c])
+    }
+
     
-    
-    
-    println!("There are {} prime numbers between 1 and {}", p, i);
+    //println!("There are {} prime numbers between 1 and {}", primes.len(), i);
     
     
 }
